0052a39614c1400793e46128a4bf265bc52ff4a3
Artyom Yuryshev <artyom_yuryshev@epam.com>
2024-09-09 12:03:25 +0300

    added mobile tests

--- src/pageObject/components/add-to-estimate-popup-component.ts
+++ src/pageObject/components/add-to-estimate-popup-component.ts
@@ -9,23 +9,35 @@ export class AdEstimatePopupComponent {
         );
     }
 
+    get mobileAddEstimateButton() {
+        return this.page
+            .locator('//button[@data-idom-class="xhASFc" or @data-idom-class="ePqIy"]')
+            .first();
+    }
+
     get addEstimationModalWindow() {
         return this.page.locator('[aria-label="Add to this estimate"]');
     }
 
+    get mobileAddEstimationModalWindow() {
+        return this.page.locator(
+            '//div[@aria-modal="true" and @role="dialog"]//span[text()="Add to this estimate"]/ancestor::div[@aria-modal="true" and @role="dialog"]',
+        );
+    }
+
     get computeEngineElement() {
-        return this.page.locator('//h2[text()="Compute Engine"]');
+        return this.page.locator('//h2[text()="Compute Engine"]/..');
     }
 
     get cloudStorageElement() {
-        return this.page.locator('//h2[text()="Cloud Storage"]');
+        return this.page.locator('//h2[text()="Cloud Storage"]/..');
     }
 
     get bigQueryElement() {
-        return this.page.locator('//h2[text()="BigQuery"]');
+        return this.page.locator('//h2[text()="BigQuery"]/..');
     }
 
     get kubernetesEngineElement() {
-        return this.page.locator('//h2[text()="Google Kubernetes Engine"]');
+        return this.page.locator('//h2[text()="Google Kubernetes Engine"]/..');
     }
 }
--- src/tests/smoke/multiple-types.spec.ts
+++ src/tests/smoke/multiple-types.spec.ts
@@ -35,4 +35,38 @@ test.describe('Cloud Calculator. 4 types Smoke', () => {
         await expect(calculatorPage.costDetails.kubernetesEngineCard).toBeVisible();
         await expect(calculatorPage.costDetails.costInDetails).toHaveText('$9,253.19');
     });
+    
+    test('@mobile | Should be able to add Compute Engine, Cloud Storage, BigQuery and Kubernetes Engine to calculator', async ({
+        calculatorPage,
+    }) => {
+        await waitAndClick(calculatorPage.addEstimatePopup.mobileAddEstimateButton);
+        await calculatorPage.addEstimatePopup.mobileAddEstimationModalWindow.waitFor();
+    
+        await waitAndClick(calculatorPage.addEstimatePopup.computeEngineElement);
+        await calculatorPage.costDetails.instanceCard.waitFor();
+    
+        await calculatorPage.addEstimatePopup.mobileAddEstimateButton.click();
+        await calculatorPage.addEstimatePopup.mobileAddEstimationModalWindow.waitFor();
+    
+        await waitAndClick(calculatorPage.addEstimatePopup.cloudStorageElement);
+        await calculatorPage.costDetails.cloudStorageCard.waitFor();
+    
+        await calculatorPage.addEstimatePopup.mobileAddEstimateButton.click();
+        await calculatorPage.addEstimatePopup.mobileAddEstimationModalWindow.waitFor();
+    
+        await waitAndClick(calculatorPage.addEstimatePopup.bigQueryElement);
+        await calculatorPage.costDetails.bigQueryEditionsCard.waitFor();
+    
+        await calculatorPage.addEstimatePopup.mobileAddEstimateButton.click();
+        await calculatorPage.addEstimatePopup.mobileAddEstimationModalWindow.waitFor();
+    
+        await waitAndClick(calculatorPage.addEstimatePopup.kubernetesEngineElement);
+        await calculatorPage.costDetails.kubernetesEngineCard.waitFor();
+    
+        await expect(calculatorPage.costDetails.instanceCard).toBeVisible();
+        await expect(calculatorPage.costDetails.cloudStorageCard).toBeVisible();
+        await expect(calculatorPage.costDetails.bigQueryEditionsCard).toBeVisible();
+        await expect(calculatorPage.costDetails.kubernetesEngineCard).toBeVisible();
+        await expect(calculatorPage.costDetails.costInDetails).toHaveText('$9,253.19');
+    });
 });
