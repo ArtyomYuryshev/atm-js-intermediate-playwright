c52b65043bafcdfb6a574443efa932b8ff3c10c7
Artyom Yuryshev <artyom_yuryshev@epam.com>
2024-09-09 21:11:12 +0300

    remove console.log from test

--- src/tests/l10n/localization.spec.ts
+++ src/tests/l10n/localization.spec.ts
@@ -4,11 +4,11 @@ import { getTextContent } from '../../utils/helpers';
 
 test.describe('Localization Tests', () => {
     Object.entries(localizationData).forEach(([language, expectedTexts]) => {
-        test(`Should display correct header and footer texts in ${language}`, async ({ calculatorPage }) => {
+        test(`Should display correct header and footer texts in ${language}`, async ({
+            calculatorPage,
+        }) => {
             await calculatorPage.headerComponent.selectLanguage(expectedTexts.value);
-
             const headerText = await getTextContent(calculatorPage.headerComponent.header);
-            console.log(headerText);
 
             expect(headerText).toBe(expectedTexts.header);
         });
