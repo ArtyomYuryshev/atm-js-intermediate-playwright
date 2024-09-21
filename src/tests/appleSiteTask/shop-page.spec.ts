12ea9fefaf069e9055c36a04eeb5930dde77d3c4
Artyom Yuryshev <artyom_yuryshev@epam.com>
2024-09-21 20:21:51 +0300

    added tests for apple.com

---
+++ src/tests/appleSiteTask/shop-page.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '@playwright/test';
+import { ApplePage } from './applePO/main-page';
+import { ProductPage } from './applePO/shop-page';
+
+const regionsWithShop = [
+    {
+        name: 'UK',
+        geoCookie: 'GB',
+        currency: '£',
+        buyNowText: 'Buy',
+        productTitle: 'iPhone 16 Pro & iPhone 16 Pro Max ',
+        productUrl: 'https://www.apple.com/uk/shop/buy-iphone',
+    },
+    {
+        name: 'JP',
+        geoCookie: 'JP',
+        currency: '円',
+        buyNowText: '購入',
+        productTitle: 'iPhone 16 Proと iPhone 16 Pro Max ',
+        productUrl: 'https://www.apple.com/jp/shop/buy-iphone',
+    },
+];
+
+regionsWithShop.forEach(({ name, geoCookie, currency, buyNowText, productUrl, productTitle }) => {
+    test.describe(`Product card for region ${name}`, () => {
+        test.beforeEach(async ({ page }) => {
+            const applePage = new ApplePage(page);
+            await applePage.setGeoCookie(geoCookie);
+        });
+
+        test(`Product card should have correct content for ${name}`, async ({ page }) => {
+            const productPage = new ProductPage(page);
+            await productPage.goto(productUrl);
+            await expect(productPage.price).toContainText(currency);
+            await expect(productPage.productTitle).toContainText(productTitle);
+            await expect(productPage.buyNowButton16Pro).toContainText(buyNowText);
+        });
+    });
+});
---
+++ src/tests/appleSiteTask/store-and-cart.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+import { ApplePage } from './applePO/main-page';
+
+const regions = [
+    { name: 'UK', geoCookie: 'GB', expectNotification: true, expectStoreAndCart: true },
+    { name: 'BY', geoCookie: 'BY', expectNotification: true, expectStoreAndCart: false },
+    { name: 'GE', geoCookie: 'GE', expectNotification: true, expectStoreAndCart: false },
+    { name: 'SG', geoCookie: 'SG', expectNotification: true, expectStoreAndCart: true },
+    { name: 'US', geoCookie: 'US', expectNotification: false, expectStoreAndCart: true },
+];
+
+regions.forEach(({ name, geoCookie, expectNotification, expectStoreAndCart }) => {
+    test.describe(`Service availability for region ${name}`, () => {
+        test.beforeEach(async ({ page }) => {
+            const applePage = new ApplePage(page);
+            await applePage.setGeoCookie(geoCookie);
+            await applePage.goto();
+        });
+
+        if (expectStoreAndCart) {
+            test(`Store and cart options should be visible for ${name}`, async ({ page }) => {
+                const applePage = new ApplePage(page);
+                await applePage.handleNotification(expectNotification);
+                await expect(applePage.storeOption).toBeVisible();
+                await expect(applePage.cartOption).toBeVisible();
+            });
+        } else {
+            test(`Store and cart options should be hidden for ${name}`, async ({ page }) => {
+                const applePage = new ApplePage(page);
+                await applePage.handleNotification(expectNotification);
+                await expect(applePage.storeOption).toBeHidden();
+                await expect(applePage.cartOption).toBeHidden();
+            });
+        }
+    });
+});
